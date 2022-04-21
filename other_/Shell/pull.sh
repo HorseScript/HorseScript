3877cfaff81529bde28d46f5bafbb270ca0285d1
gemsvido <gemsvido@gmail.com>
2022-04-21 10:26:02 -0700

    utility
    Signed-off-by: gemsvido <gemsvido@gmail.com>

--- other_/Shell/pull.sh
+++
@@ -1,4 +0,0 @@
-git pull origin master
-
-java -jar other/Resources/pull_success.jar
-# pull_success.jar source: https://github.com/afkvido/TextEntry-Popup
--- other_/Windows/push.bat
+++
@@ -1,2 +0,0 @@
-java -jar other/Resources/push.jar
-:: push.jar source: https://github.com/afkvido/TextEntry-Popup
