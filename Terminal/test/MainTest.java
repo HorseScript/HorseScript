19c60e8d625145df7ecdac9d4d9d362a70b42985
gemsvido <gemsvido@gmail.com>
2022-05-10 09:52:05 -0700

    Automatic Testing
    coming soonTM

---
+++ Terminal/test/MainTest.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2022, HorseScript
+ * All rights reserved.
+ */
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test void main() {
+    }
+
+    /** Utility class <b>MainTest</b> cannot be initialized. */
+    private MainTest () {}
+}
---
+++ Terminal/test/TerminalTest.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2022, HorseScript
+ * All rights reserved.
+ */
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TerminalTest {
+
+    @Test void main() {
+    }
+
+    @Test void runTerm() {
+    }
+
+    @Test void evaluate_terminal() {
+    }
+
+    @Test void horse() {
+    }
+
+    @Test void evaluator() {
+    }
+
+    @Test void console() {
+    }
+
+    /** Utility class <b>TerminalTest</b> cannot be initialized. */
+    private TerminalTest () {}
+}
